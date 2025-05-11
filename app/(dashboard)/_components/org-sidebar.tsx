c0d8cdc47905298e31183821ed990b498f3c6371
Some Great <black13dog@yandex.ru>
2025-05-11 11:09:13 +0300

    baaaaasic dashboard layout

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const OrgSidebar = () => {
+    return (
+        <div className="hidden lg:flex flex-col space-y-6 w-[210px] pl-5 pt-5 bg-red-500">
+            <h1 className="text-black font-bold">Org Sidebar</h1>
+        </div>
+    );
+}
