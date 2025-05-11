b4af120064e48911d2816e2051e0656320319289
Some Great <black13dog@yandex.ru>
2025-05-11 20:07:51 +0300

    navbar

---
+++ app/(dashboard)/_components/invite-button.tsx
@@ -0,0 +1,23 @@
+import { Plus } from "lucide-react";
+import { OrganizationProfile } from "@clerk/nextjs";
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+export const InviteButton = () => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline">
+                    <Plus className="h-4 w-4 mr-2" />
+                    Invite members
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[930px] justify-center">
+                <DialogTitle className="text-lg font-semibold">
+                    Invite members
+                </DialogTitle>
+                <OrganizationProfile routing="hash"/>
+            </DialogContent>
+        </Dialog>
+    );
+};
