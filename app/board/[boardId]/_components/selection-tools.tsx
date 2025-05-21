9b2682a8008f9449c8b788b2ff6c691c7df802aa
Some Great <black13dog@yandex.ru>
2025-05-21 16:02:25 +0300

    easy send to back and move to front

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -8,7 +8,7 @@ import { ColorPicker } from "./color-picker";
 import { useDeleteLayers } from "@/hooks/use-delete-layers";
 import { Button } from "@/components/ui/button";
 import { Hint } from "@/components/hint";
-import { Trash2 } from "lucide-react";
+import { BringToFront, SendToBack, Trash2 } from "lucide-react";
 
 interface SelectionToolsProps {
   camera: Camera;
@@ -20,6 +20,39 @@ export const SelectionTools = memo(
     const selection = useSelf((me) => me.presence.selection ?? []);
     const bounds = useActiveSelectionArea();
     const deleteLayers = useDeleteLayers();
+
+const moveToBack = useMutation(({ storage }) => {
+  const liveLayerIds = storage.get("layerIds");
+  const arr = liveLayerIds.toArray();
+  const indices = arr
+    .map((id, idx) => ({ id, idx }))
+    .filter(item => selection.includes(item.id))
+    .map(item => item.idx)
+    .sort((a, b) => a - b);
+
+  
+  indices.forEach((origIdx, i) => {
+    liveLayerIds.move(origIdx, i);
+  });
+}, [selection]);
+
+
+const moveToFront = useMutation(({ storage }) => {
+  const liveLayerIds = storage.get("layerIds");
+  const arr = liveLayerIds.toArray();
+  
+  const indices = arr
+    .map((id, idx) => ({ id, idx }))
+    .filter(item => selection.includes(item.id))
+    .map(item => item.idx)
+    .sort((a, b) => b - a);
+  const length = arr.length;
+  indices.forEach((origIdx, i) => {
+    liveLayerIds.move(origIdx, length - 1 - i);
+  });
+}, [selection]);
+
+
     const setFill = useMutation(
       ({ storage }, fill: Color) => {
         const liveLayers = storage.get("layers");
@@ -46,12 +79,24 @@ export const SelectionTools = memo(
 
     return (
       <div
-        className="absolute p-3 rounded-xl bg-white shadow-sm border flex select-none"
+        className="absolute p-3 rounded-xl bg-white shadow-sm border flex items-center select-none"
         style={{
           transform: `translate(calc(${x}px - 50%), calc(${y - 10}px - 100%))`,
         }}
       >
         <ColorPicker onChange={setFill} />
+        <div className="flex flex-col gap-y-0.5 justify-center ml-3">
+          <Hint label="bring to front">
+            <Button variant="ghost" size="icon" onClick={moveToFront}>
+              <BringToFront />
+            </Button>
+          </Hint>
+          <Hint label="send to back" side="bottom">
+            <Button variant="ghost" size="icon" onClick={moveToBack}>
+              <SendToBack />
+            </Button>
+          </Hint>
+        </div>
         <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
           <Hint label="delete" side="right" sideOffset={10}>
             <Button
